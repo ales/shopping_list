26e166bc15da77238670c754681198ff55bf2c38
Ales Novotny <novotny@divine.cz>
2009-12-03 00:04:12 +0100

    seedy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+c = Category.create :title => 'pečiva'
+c.items.create [{:title => 'rohlíky'}, {:title => 'chleba'}, {:title => 'veku'}, {:title => 'borůvkový zákusek'}]
+
+c = Category.create :title => 'těstovin'
+c.items.create [{:title => 'špagety'}, {:title => 'vrtulky'}, {:title => 'pesto'}, {:title => 'kečup'}]
+
+c = Category.create :title => 'zeleniny'
+c.items.create [{:title => 'mrkev'}, {:title => 'petržel'}, {:title => 'lilek'}, {:title => 'zelenou papriku'}, {:title => 'rajčata'}]
+
+c = Category.create :title => 'ovoce'
+c.items.create [{:title => 'jablka'}, {:title => 'hrušky'}, {:title => 'kaki'}]
